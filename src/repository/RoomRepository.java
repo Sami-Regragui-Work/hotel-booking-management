722ed0d01414dbfe46967450e71062bfa046ed79
Sami-Regragui-Work <sami-regragui-work@protonmail.com>
2026-09-08 16:06:30 +0100

    feat: add repositories

---
+++ src/repository/RoomRepository.java
@@ -0,0 +1,12 @@
+package repository;
+
+import model.Room;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RoomRepository {
+    void save(Room room);
+    Optional<Room> findByRoomNumber(String roomNumber);
+    List<Room> findAll();
+}
---
+++ src/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package repository;
+
+import model.User;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository {
+    void save(User user);
+    Optional<User> findById(UUID id);
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+    List<User> findAll();
+}
